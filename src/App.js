235b09515f460097830d6441483614c047c48e28
Patric Lundin <Palu92@hotmail.com>
2018-10-11 20:34:02 +0200

    Add valueSquare factory function and onChange handler

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import Square from './components/Square';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    value1: 0,
+    value2: 0,
+    value3: 0
+  }
+
+  onValueChangeHandler = number => eventValue => {
+    this.setState(prevState => {
+      return {
+        ["value" + number]: eventValue
+      }
+    })
+  }
+
+  createValueSquare = (number) => {
+    return (
+      <Square key={number}>
+        <p>Value {number}:</p>
+        <input type="number" value={this.state["value" + number]} onChange={event => this.onValueChangeHandler(number)(event.target.value)}/>
+      </Square>
+    )
+  }
+
   render() {
+    const valueSquares = [...Array(3)].map((x, i) => this.createValueSquare(i + 1))
     return (
       <div className="App">
-        <Square>
-
-        </Square>
+        {valueSquares}
       </div>
     );
   }
